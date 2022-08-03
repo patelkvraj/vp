c8bb4f9e754c2d857bd5a42cd8f11bab10517b0a
Vraj Patel <mycsstuff@outlook.com>
2022-08-02 20:16:43 -0400

    Assigned a function giveAlert() to the button (Contact section).

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function backgroundChange() {
-    document.getElementById("hero").style.backgroundColor="blue";
+/*                          THE DOCUMENTATION
+
+Dummy comment for function
+--------------------------
+Purpose: The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
+Location: The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+
+
+
+*/
+
+
+/*
+Purpose: The function will give an alert when the user will try to use the form service. 
+Location: The function is use in the CONTACT section. 
+*/
+function giveAlert() {
+    alert("The service is currently unavailable. I am still working on it. Sorry for the inconvenience.");
 }
