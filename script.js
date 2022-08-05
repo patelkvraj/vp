2d0922e865b07d45960f0e991f18dc48bbb41aad
Vraj Patel <mycsstuff@outlook.com>
2022-08-05 18:48:37 -0400

    Added a function that counts the total number of months passed since you first started learning the language.

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+
 /*                          THE DOCUMENTATION
 
 Dummy comment for function
 --------------------------
 Purpose: The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
+Function call: The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
 Location: The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
 
+Dummy comment for if-else/if statements
+--------------------------
+Purpose: The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
 
 
 */
 
 
+//----------------------------------------------------------------------------------------------------------------
 /*
 Purpose: The function will give an alert when the user will try to use the form service. 
+Function call: index.html file.
 Location: The function is use in the CONTACT section. 
 */
 function giveAlert() {
@@ -19,42 +26,39 @@ function giveAlert() {
 }
 
 
-
+//          PROBLEM: Update will not work after this december.
 /*-------------------------------------- Programming Languages Experience ------------------------------------- */
+const current__date = new Date();   // created object holds current date
+const start__date__html = new Date();  
+start__date__html.setUTCDate(1);  // the date I started learning HTML
+start__date__html.setUTCMonth(1); // the month I started learning HTML 
+start__date__html.setUTCFullYear(2022); // the year I started learning HTML
 
-/* ---------- HTML ---------- */
-const start__html = new Date();
-const current__html = new Date();
-start__html.setUTCMonth(2); // started learning html in February 2022
+document.getElementById("html__langExp--hero").innerHTML = calculateMonths(start__date__html.getUTCMonth());
 
-// if (current__html == 12)
-
-
-/* HTML Experience calculation */
-var exp__html = (1 + current__html.getUTCMonth()) - start__html.getUTCMonth(); // Current month - Month started learning html 
-
-document.getElementById("html__langExp--hero").innerHTML = exp__html;
 
 /* ---------- CSS ---------- */
-const start__css = new Date();
-const current__css = new Date();
-start__css.setUTCMonth(2); // started learning html in February 2022
+const start__date__css = new Date();
+start__date__css.setUTCDate(1);  // the date I started learning CSS
+start__date__css.setUTCMonth(1); // the month I started learning CSS
+start__date__css.setUTCFullYear(2022);   // the year I started learning CSS
 
-/* HTML Experience calculation */
-var exp__html = (1 + current__css.getUTCMonth()) - start__css.getUTCMonth(); // Current month - Month started learning html 
+document.getElementById("css__langExp--hero").innerHTML = calculateMonths(start__date__css.getUTCMonth());
 
-document.getElementById("css__langExp--hero").innerHTML = exp__html;
 
 /* ---------- JavaScript ---------- */
-const start__javaScript= new Date();
-const current__javaScript = new Date();
-start__javaScript.setUTCMonth(7); // started learning html in February 2022
+const start__date__javaScript = new Date();
+start__date__javaScript.setUTCDate(1);   // the date I started learning JavaScript
+start__date__javaScript.setUTCMonth(7);   // the month I started learning JavaScript
+start__date__javaScript.setUTCFullYear(2022);   // the year I started learning JavaScript
 
-/* HTML Experience calculation */
-var exp__html = (1 + current__javaScript.getUTCMonth()) - start__javaScript.getUTCMonth(); // Current month - Month started learning html 
+document.getElementById("javaScript__langExp--hero").innerHTML = calculateMonths(start__date__javaScript.getUTCMonth());
 
-document.getElementById("javaScript__langExp--hero").innerHTML = exp__html;
-
-
-
-
+/*
+Purpose: Gives the total number of months passed since learning html. (You started learning html: Feb 2022)
+Function call: script.js file.
+Location: The function is use in the "Programming Languages Experience" section. 
+*/
+function calculateMonths(x) {
+    return ((1 + current__date.getUTCMonth()) - (1 + x))
+}
