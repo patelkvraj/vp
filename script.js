a230d6e362c73bb73c6d17205a215b9b2d5c6cab
Vraj Patel <mycsstuff@outlook.com>
2023-01-21 10:51:03 -0500

    I changed the name of monthExperience() method to languageExperience().

--- script.js
+++ script.js
@@ -20,36 +20,14 @@ function giveAlert() {
 // start__date__html.setUTCMonth(2);       // the month I started learning HTML 
 // start__date__html.setUTCFullYear(2022); // the year I started learning HTML
 
-document.getElementById("html__languageExperience--hero").innerHTML = monthExperience(2022, 2, 1); // the date I started learning HTML
-document.getElementById("css__languageExperience--hero").innerHTML = monthExperience(2022, 2, 1); // the date I started learning CSS
-document.getElementById("javaScript__languageExperience--hero").innerHTML = monthExperience(2022, 8, 1); // the date I started learning JavaScript
+document.getElementById("html__languageExperience--hero").innerHTML = languageExperience(2022, 2, 1);          // the date I started learning HTML
+document.getElementById("css__languageExperience--hero").innerHTML = languageExperience(2022, 2, 1);           // the date I started learning CSS
+document.getElementById("javaScript__languageExperience--hero").innerHTML = languageExperience(2022, 8, 1);    // the date I started learning JavaScript
+// document.getElementById("CPlusPlus__languageExperience--hero").innerHTML = monthExperience(2020, 9, 1);     // the date I started learning C++
 
-
-//          PROBLEM: Update will not work after this december.
-// /*-------------------------------------- Programming Languages Experience ------------------------------------- */
-// const current__date = new Date();   // object holds the current date
 
 // /* ---------- HTML ---------- */
 // // (You started learning html: Feb 2022)
-// const start__date__html = new Date();  
-// start__date__html.setUTCDate(1);  // the date I started learning HTML
-// start__date__html.setUTCMonth(1); // the month I started learning HTML 
-// start__date__html.setUTCFullYear(2022); // the year I started learning HTML
-// document.getElementById("html__langExp--hero").innerHTML = calculateMonths(start__date__html.getUTCMonth());
-
-// /* ---------- CSS ---------- */
-// const start__date__css = new Date();
-// start__date__css.setUTCDate(1);  // the date I started learning CSS
-// start__date__css.setUTCMonth(1); // the month I started learning CSS
-// start__date__css.setUTCFullYear(2022);   // the year I started learning CSS
-// document.getElementById("css__langExp--hero").innerHTML = calculateMonths(start__date__css.getUTCMonth());
-
-// /* ---------- JavaScript ---------- */
-// const start__date__javaScript = new Date();
-// start__date__javaScript.setUTCDate(1);   // the date I started learning JavaScript
-// start__date__javaScript.setUTCMonth(7);   // the month I started learning JavaScript
-// start__date__javaScript.setUTCFullYear(2022);   // the year I started learning JavaScript
-// document.getElementById("javaScript__langExp--hero").innerHTML = calculateMonths(start__date__javaScript.getUTCMonth());
 
 // /*
 // Name            :   calculateMonths()
@@ -63,7 +41,7 @@ document.getElementById("javaScript__languageExperience--hero").innerHTML = mont
 //     return ((1 + current__date.getUTCMonth()) - (1 + x))
 // }
 
-function monthExperience(year, month, day){
+function languageExperience(year, month, day){
     /* Elapsed time between learn(When I learned a language) and present time */
     const learn_time = new Date(year, month, day, 0, 0);                                //  the date when learned a language
     const present_full_date = new Date();                                               //  now
