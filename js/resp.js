a3d7d3afd1ebc38d201f366423a351d36ddc1cdf
Vraj Patel <mycsstuff@outlook.com>
2023-02-13 20:33:54 -0500

    Header: Added a event listener to a burger.

---
+++ js/resp.js
@@ -0,0 +1,10 @@
+burger =  document.querySelector('.burger')
+logo =  document.querySelector('.container--logo')
+nav =  document.querySelector('.nav')
+nav__list =  document.querySelector('.nav__list')
+
+burger.addEventListener('click', ()=>{
+    nav.classList.toggle('h-nav-resp');
+    logo.classList.toggle('v-class-resp');
+    nav__list.classList.toggle('v-class-resp');
+})
