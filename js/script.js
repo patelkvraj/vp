b5d4a471d50f629f89f6ea8cf8b51e5baf8f7181
Vraj Patel <mycsstuff@outlook.com>
2024-08-22 22:12:49 -0400

    index.html > Contact : Changed the name of the class to envelope_close. Added 2 <i> elements with classes envelope_open and envelope_open_text.

--- js/script.js
+++ js/script.js
@@ -60,3 +60,14 @@ let year = d.getFullYear();
 const copyright__year = document.getElementById("copyright__year");
 copyright__year.innerHTML = year;
 
+/*
+Purpose: Change the envelope
+*/
+function changeEnvelope() {
+    envelope_open = document.getElementById("envelope_open");
+    envelope_open.setAttribute('style', 'display:none !important');
+    envelope_open_text = document.getElementById("envelope_open_text");
+    envelope_open_text.setAttribute('style', 'display:inline !important');
+    envelope_open_text = document.getElementById("envelope_open_text");
+    envelope_open_text.setAttribute('style', 'display:none !important');
+}
